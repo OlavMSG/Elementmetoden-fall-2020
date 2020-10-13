b488e2d5ddbb9722d18af9fe4be483b4f3193b8d
OlavMSG <olav@schmitt-gran.net>
2020-10-13 14:25:46 +0200

    readme.txt and restuctere main.py

--- Project part 1/main.py
+++ Project part 1/main.py
@@ -15,10 +15,12 @@ from Poisson2D import Dirichlet2D, Mixed2D
 
 # Parameters
 # ----------
+# N_list : list of ints
+#     list of different N-s.
 # N : int
 #     Number of nodes in the mesh.
 # f : function pointer
-#     pointer to function to be equal to on the rigth hand side.
+#     pointer to function to be equal to on the right hand side.
 # u_exact : function pointer
 #     Pointer to the function for the exact solution.
 # g_N : function pointer
@@ -26,34 +28,38 @@ from Poisson2D import Dirichlet2D, Mixed2D
 # g_D : function pointer
 #     pointer to function for the Dirichlet B.C.
 # DoSingularityCheck : bool, optional
-#     Call the singularity_check function to check i A is singular before implimetation of B.C. The default is True.
+#     Call the singularity_check function to check i A is singular before implementation of B.C. The default is True.
 # save : bool, optional
 #     Will the plot be saved in the plot folder. The default is False.
 #     Note: the plot folder must exist!
 
-# test the Gauss Quadratue script
-Gauss_quadrature_tester()
 
-# save the plots?
-save = True
-# Do the Singularity check?
-DoSingularityCheck = True
 # list of N to plot for
 N_list = [100, 500, 1000]
-
-# make a meshplot
-meshplot(N_list, save=save)
 
 # source function
 f = lambda x, y: 16 * np.pi * np.pi * (x * x + y * y) * np.sin(2 * np.pi * (x * x + y * y)) \
                       - 8 * np.pi * np.cos(2 * np.pi * (x * x + y * y))
-# Neumann B.C. function on x^2 + y^2 = r^2 = 1
-# 4 * np.pi  * np.sqrt(x * x + y * y) * np.cos(2 * np.pi * (x * x + y * y)) = 4 * np.pi on x^2 + y^2 = r^2 = 1
-g_N = lambda x, y: 4 * np.pi
-# Dirichlet B.C function
-g_D = lambda x, y: np.zeros_like(x)
 # exact solution
 u_exact = lambda x, y:  np.sin(2 * np.pi * (x * x + y * y))
+
+# Neumann B.C. function on x^2 + y^2 = r^2 = 1, y>0
+# 4 * np.pi  * np.sqrt(x * x + y * y) * np.cos(2 * np.pi * (x * x + y * y)) = 4 * np.pi on x^2 + y^2 = r^2 = 1
+g_N = lambda x, y: 4 * np.pi
+
+# Dirichlet B.C function on x^2 + y^2 = r^2 = 1 or x^2 + y^2 = r^2 = 1, y<=0
+g_D = lambda x, y: np.zeros_like(x)
+
+# Do the Singularity check?
+DoSingularityCheck = True
+# save the plots?
+save = False
+
+# test the Gauss Quadratue script
+Gauss_quadrature_tester()
+
+# make a meshplot
+meshplot(N_list, save=save)
 
 # Do the computations and plotting for all N in N_list
 for N in N_list:
---
+++ Project part 1/readme.txt
@@ -0,0 +1,30 @@
+Our project part 1 consists of the following files
+TMA4220___Project_1.pdf    : The repport.
+readme.txt                 : What you are reading now.
+getdisc.py                 : Mesh code from https://wiki.math.ntnu.no/tma4220/2020h/project given to us.
+Gauss_quadrature.py        : Script for doing 1D, 1D lineintegral and 2D integrals using Gaussian quadrature.
+contor_and_mesh_plotter.py : Script containing the functions to plot meshes and contourplots of the numerical solution, ecaxt solution and absolute 				value of the error(absoulute error).
+Poisson2D.py               : Script to solve the 2D Poisson problem using Dirichlet and Mixed boundary conditions as decribed in the project description
+main.py                    : The main script to contoroll every thing from.
+
+To use the program run the main.py script. 
+The Parameters are:
+Parameters
+----------
+N_list : list of ints
+    list of different N-s.
+	N : int
+    	Number of nodes in the mesh.
+f : function pointer
+    pointer to function to be equal to on the right hand side.
+u_exact : function pointer
+    Pointer to the function for the exact solution.
+g_N : function pointer
+     pointer to function for the Neumann B.C.
+g_D : function pointer
+    pointer to function for the Dirichlet B.C.
+DoSingularityCheck : bool, optional
+    Call the singularity_check function to check i A is singular before implementation of B.C. The default is True.
+save : bool, optional
+    Will the plot be saved in the plot folder. The default is False.
+    Note: the plot folder must exist!
